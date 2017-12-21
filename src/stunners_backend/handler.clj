97c2991b4d0f934971261d728afd0c4fc244cf03
David Martin <dwmartin41@gmail.com>
2017-12-21 19:20:12 +0000

    updated datomic schema

--- src/stunners_backend/handler.clj
+++ src/stunners_backend/handler.clj
@@ -14,11 +14,95 @@
                  :service "peer-server"
                  :access-key "myaccesskey"})))
 
-(def schema [{:db/ident :user/name
-              :db/valueType :db.type/string
-              :db/cardinality :db.cardinality/one}])
+(def user-schema [{:db/ident :user/name
+                   :db/valueType :db.type/string
+                   :db/cardinality :db.cardinality/one}
+                  {:db/ident :user/avatar
+                   :db/valueType :db.type/string
+                   :db/cardinality :db.cardinality/one}
+                  {:db/ident :user/email
+                   :db/valueType :db.type/string
+                   :db/cardinality :db.cardinality/one}
+                  {:db/ident :user/phone-number
+                   :db/valueType :db.type/string
+                   :db/cardinality :db.cardinality/one}])
 
-(<!! (d/transact conn {:tx-data schema}))
+(def stylist-schema [{:db/ident :stylist/headline
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/one}
+                     {:db/ident :stylist/images
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/many}
+                     {:db/ident :stylist/description
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/one}])
+
+(def location-schema [{:db/ident :location/address
+                       :db/valueType :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident :location/outcode
+                       :db/valueType :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident :location/postcode
+                       :db/valueType :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident :location/lat
+                       :db/valueType :db.type/double
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident :location/lng
+                       :db/valueType :db.type/double
+                       :db/cardinality :db.cardinality/one}])
+
+(def filter-group-schema [{:db/ident :filter-group/label
+                           :db/valueType :db.type/string
+                           :db/cardinality :db.cardinality/one}
+                          {:db/ident :filter-group/product-types
+                           :db/valueType :db.type/ref
+                           :db/cardinality :db.cardinality/many}])
+
+(def product-type-schema (->> [:haircut :waxing :nails]
+                              (map #(keyword "product-type" (name %)))
+                              (map #(hash-map :db/ident %))))
+
+(def product-schema [{:db/ident :product/type
+                      :db/valueType :db.type/ref
+                      :db/cardinality :db.cardinality/one}
+                     {:db/ident :product/cost
+                      :db/valueType :db.type/double
+                      :db/cardinality :db.cardinality/one}
+                     {:db/ident :product/stylist
+                      :db/valueType :db.type/ref
+                      :db/cardinality :db.cardinality/one}])
+
+(def appointment-schema [{:db/ident :appointment/time
+                          :db/valueType :db.type/instant
+                          :db/cardinality :db.cardinality/one}
+                         {:db/ident :appointment/stylist
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/one}
+                         {:db/ident :appointment/stylee
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/one}
+                         {:db/ident :appointment/product-types
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/many}
+                         {:db/ident :appointment-status/pending}
+                         {:db/ident :appointment-status/confirmed}
+                         {:db/ident :appointment-status/rejected}
+                         {:db/ident :appointment/status
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/many}])
+
+(def schema (concat user-schema
+                    location-schema
+                    product-type-schema
+                    product-schema
+                    appointment-schema
+                    stylist-schema
+                    filter-group-schema))
+
+
+;(<!! (d/transact conn {:tx-data schema}))
 
 (defroutes app-routes
   (GET "/health-check" [] {:status 200
