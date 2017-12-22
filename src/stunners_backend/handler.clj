a7892702e082ddaf1782b93229529029fba8ab8c
David Martin <dwmartin41@gmail.com>
2017-12-22 13:48:43 +0000

    added :user/auth0-id onto request map in auth middleware

--- src/stunners_backend/handler.clj
+++ src/stunners_backend/handler.clj
@@ -127,19 +127,21 @@
 
 
 (defn authenticate [handler credentials]
-  "Authenticates using auth token in Authorization header, or query param if header not set. Adds the auth token onto the request as :token"
+  "Authenticates using auth token in Authorization header, or query param if header not set. Adds the auth token onto the request as :token, and the user's auth0 id as :user/auth0-id"
   (fn [request]
     (let [{:keys [headers query-params]} request
           token (-> (get headers "authorization")
                     (or "")
                     (str/replace #"^\s*Bearer\s+" ""))
-          valid? (jwt/valid? token
-                             (jwt/signature (sign/hs256 (:client-secret credentials)))
-                             (jwt/aud (:client-id credentials))
-                             (jwt/iss (:domain credentials))
-                             jwt/exp)]
-      (if valid?
-        (handler (assoc request :token token))
+          decoded-token (jwt/validate token
+                                      (jwt/signature (sign/hs256 (:client-secret credentials)))
+                                      (jwt/aud (:client-id credentials))
+                                      (jwt/iss (:domain credentials))
+                                      jwt/exp)]
+      (if-let [{:keys [sub]} decoded-token]
+        (handler (-> request
+                     (assoc :token token)
+                     (assoc :user/auth0-id sub)))
         {:status 401
          :headers {"Content-Type" "application/edn"}
          :body (pr-str {:message "Auth token not found or is invalid"})}))))
@@ -148,7 +150,7 @@
   (GET "/health-check" [] {:status 200
                            :headers {"Content-Type" "application/edn"}
                            :body (pr-str {:status :ok})})
-  (GET "/stylists" []
+  (GET "/stylists" []       
        (->> (q '[:find (pull ?s [:user/name :location/outcode :stylist/headline :stylist/images :stylist/description :db/id :user/avatar {:product/_stylist [:product/cost {:product/type [:db/ident]}]}])
                  :where [?s :stylist/headline]])
             inline-enums
