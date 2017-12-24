059142f6c6c4ba9189eb3760c9aeb8251289fb92
David Martin <dwmartin41@gmail.com>
2017-12-24 15:27:31 +0000

    spec for appointment POST request body

--- src/stunners_backend/specs.clj
+++ src/stunners_backend/specs.clj
@@ -5,6 +5,8 @@
 (s/def :request/user (s/keys :req [:user/name :user/email :user/avatar :user/phone-number :user/auth0-id]
                              :opt [:location/address]))
 
+(s/def :request/appointment (s/keys :req [:location/lat :location/lng :appointment/stylist :appointment/time :appointment/product-types]))
+
 (s/def :user/name (s/and string? (complement str/blank?)))
 
 (def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
@@ -22,3 +24,14 @@
 (s/def :location/address (s/and string? (complement str/blank?)))
 
 (s/def :user/auth0-id (s/and string? (complement str/blank?)))
+
+(s/def :appointment/status #{:appointment-status/pending :appointment-status/confirmed :appointment-status/rejected})
+
+(s/def :appointment/product-types (s/coll-of #{:product-type/haircut :product-type/waxing :product-type/nails} :min-count 1 :distinct true))
+
+(s/def :appointment/time inst?)
+
+(s/def :location/lat (s/and number? #(<= -90 % 90)))
+(s/def :location/lng (s/and number? #(<= -180 % 180)))
+
+(s/def :appointment/stylist pos-int?)
