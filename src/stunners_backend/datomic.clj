f39729a4c30824f6c586d2d8bd3194d8368bcbbe
David Martin <dwmartin41@gmail.com>
2018-05-29 22:57:47 +0100

    updated connection string

--- src/stunners_backend/datomic.clj
+++ src/stunners_backend/datomic.clj
@@ -5,7 +5,7 @@
             [stunners-backend.schema :refer [schema]]))
 
 
-(def db-uri "datomic:sql://stunners?jdbc:postgresql://ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/d4buk3vuuj5442?user=qwjavacktkbhgo&password=654b207e5679f6cbd9684eb3411b82d16c4dc831d1a7cd213a7d48495ed4c883&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" #_"datomic:dev://localhost:4334/stunners")
+(def db-uri "datomic:sql://stunners?jdbc:postgresql://ec2-54-217-217-194.eu-west-1.compute.amazonaws.com:5432/dd5rk21rc395se?user=qnbfniesbkxoxq&password=5a4b6bd7099b9154d9213fa971a1f43624f3379f31cadc02b4b971a010c9b689&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" #_"datomic:dev://localhost:4334/stunners")
 
 (defstate conn
   :start (do (d/create-database db-uri)
